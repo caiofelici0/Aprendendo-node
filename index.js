ac90a91ae6a7c5cd832a3dd144b577889a1ed20d
Caio Felicio <caiofelicio1711@gmail.com>
2023-06-09 16:27:03 -0300

    userModel e requests

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// const { Person } = require("./person");
-// const person = new Person("Caio");
-
 const dotenv = require("dotenv");
 dotenv.config();
+
+const connectToDatabase = require("./src/database/connect");
+connectToDatabase();
 
 // require("./modules/path");
 // require("./modules/fs");
 // require("./modules/http");
 require("./modules/express");
-require("./src/database/connect");
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,22 +1,14 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@aprendendonode.yt3d4br.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Sucesso ao conectar com o MongoDB");
-  } finally {
-    await client.close();
-  }
+const connectToDatabase = async () => {
+ try {
+  await mongoose.connect(uri);
+  console.log("Sucesso ao conectar com o MongoDB");
+ } catch (error) {
+  console.log("Erro ao se conectar ao banco", error.mensage);
+  process.exit(1);
+ }
 }
 
-run().catch(console.dir);
+module.exports = connectToDatabase;
